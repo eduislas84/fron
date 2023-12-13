66d070c27db25cb8c46d6648fbec9cdcbb9aca50
eduislas84 <122810400+eduislas84@users.noreply.github.com>
2023-12-13 10:13:13 -0600

    Update get_all.js

--- static/js/get_all.js
+++ static/js/get_all.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "https://back-heroku-cb17d0009ad0.herokuapp.com";
+const SERVER_URL = "ttps://backendxd-4eb98d5ef344.herokuapp.com";
 const CONTACTS_ENDPOINT = "/contactos";
 
 async function getAll() {
